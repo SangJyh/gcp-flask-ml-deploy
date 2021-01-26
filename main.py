1dd45bb79d23cc141a8d562d2d71d6bf3534e89d
Sang-Jyh Lin <52944419+SangJyh@users.noreply.github.com>
2021-01-26 13:09:55 -0500

    test cd

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def hello():
     """Return a friendly HTTP greeting."""
     print("I am inside hello world")
-    return 'Hello World! <br> hi! I am using emacs'
+    return 'Hello World! <br> hi! I am using emacs <br> continue test CI/CD'
 
 @app.route('/echo/<name>')
 def echo(name):
